886adb05bd3a942f0fda5f258f624596605fb82e
sigmaray <sigmaray@gmail.com>
2018-07-04 01:16:46 +0300

    Refactoring

--- src/flickr.js
+++ src/flickr.js
@@ -5,18 +5,13 @@ var PER_PAGE = 20;
 
 // @param function(data) successCallback
 // @param function(errorMessage) errorCallback
-// @param string pic - Search query for flickr.
+// @param string searchQuery | search query for flickr.
 // @param string page = 1 
 // @param string per_page = PER_PAGE
-export default function downloadPics(successCallback, errorCallback, pic = '', page = 1, per_page = PER_PAGE) {
-  var imgs = [];
-  let url = '';
-  if (pic) {
-    url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + API_KEY + '&text=' + pic + '&safe_search=1&page=' + page + '&per_page=' + per_page;
-  }
-  else {
-    url = 'https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=' + API_KEY + '&safe_search=1&page=' + page + '&per_page=' + per_page;
-  }
+export default function downloadPics(successCallback, errorCallback, searchQuery = '', page = 1, per_page = PER_PAGE) {
+  let imgs = [];
+  const method = searchQuery ? 'flickr.photos.search' : 'flickr.photos.getRecent';
+  const url = `https://api.flickr.com/services/rest/?method=${method}&api_key=${API_KEY}&text=${searchQuery}&safe_search=1&page=${page}&per_page=${per_page}`;
   $.ajax({
     dataType: "json",
     url: url + '&format=json&jsoncallback=?',
@@ -25,12 +20,12 @@ export default function downloadPics(successCallback, errorCallback, pic = '', p
         errorCallback(JSON.stringify(["Problem with data", data]));
       } else {
         $.each(data.photos.photo, function(i, item) {
-          var flickrUrl, src;
-          // src = 'http://farm' + item.farm + '.static.flickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_m.jpg'
-          src = 'http://farm' + item.farm + '.static.flickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_h.jpg';
-          // bigImageSrc = 'http://farm' + item.farm + '.static.flickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_c.jpg'
-          // bigImageSrc = 'http://farm' + item.farm + '.static.flickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_h.jpg'
-          flickrUrl = 'https://www.flickr.com/photos/' + item.owner + '/' + item.id;
+          let flickrUrl, src;
+          // const size = `c`;
+          // const size = `c`;
+          const size = `h`;
+          src = `http://farm${item.farm}.static.flickr.com/${item.server}/${item.id}_${item.secret}_${size}.jpg`;
+          flickrUrl = `https://www.flickr.com/photos/${item.owner}/${item.id}`;
           imgs.push({
             src: src,
             flickrUrl: flickrUrl
