4fb3e296109936eaf01dfa4f88bfb1706f97c255
Stanislav Zaleski <sigmaray@gmail.com>
2019-05-25 03:34:17 +0300

    Fix deploy

--- deploy.js
+++ deploy.js
@@ -1,6 +1,6 @@
 var ghpages = require('gh-pages');
 
 ghpages.publish('build',
-  {repo: 'https://github.com/sigmaray/freact/'},
+  {repo: 'https://github.com/sigmaray/react-flickr-slideshow-demo/'},
   function(err) {console.log(err)}
 );
